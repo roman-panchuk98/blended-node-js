f1b4c2cf08e7010619299c77de1e4b88eccb90c4
Beowulf3 <vk3volkov@gmail.com>
2025-11-09 10:57:00 +0200

    add get products and product by id

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,1 +1,12 @@
+import { Router } from 'express';
+import {
+  getProductById,
+  getProducts,
+} from '../controllers/productsController.js';
 
+const router = Router();
+
+router.get('/products', getProducts);
+router.get('/products/:productId', getProductById);
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -5,12 +5,15 @@ import cors from 'cors';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
+import productsRouter from './routes/productsRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3030;
 
 app.use(express.json());
 app.use(cors());
+
+app.use(productsRouter);
 
 app.use(notFoundHandler);
 
