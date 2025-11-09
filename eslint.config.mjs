d6cac7aa93aef010f7878855ce869010a9c5d8e6
Roman Panchuk <panchuk.roman98@gmail.com>
2025-11-09 09:38:27 +0100

    Task 1 and 2

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    rules: {
+      semi: 'error',
+      'no-unused-vars': ['error', { args: 'none' }],
+      'no-undef': 'error',
+    },
+  },
+
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: { globals: globals.node },
+  },
+]);
---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+
+const app = express();
+const PORT = process.env.PORT ?? 3030;
+
+app.use(express.json());
+app.use(cors());
+
+app.use(notFoundHandler);
+
+app.use(errorHandler);
+
+await connectMongoDB();
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
