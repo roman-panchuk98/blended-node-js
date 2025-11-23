197b82d3f4dc5c4da9f3e1172c297bfba8aadf39
nasti <anastasiasaienko634@gmail.com>
2025-11-23 10:30:45 +0100

    task-3

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { registerUserSchema } from '../validation/validation.js';
-import { registerUser } from '../controllers/authController.js';
+import {
+  loginUserSchema,
+  registerUserSchema,
+} from '../validation/validation.js';
+import { loginUser, registerUser } from '../controllers/authController.js';
 const router = Router();
 
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
-
+router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 export default router;
--- src/validation/validation.js
+++ src/validation/validation.js
@@ -7,3 +7,10 @@ export const registerUserSchema = {
     password: Joi.string().required(),
   }),
 };
+
+export const loginUserSchema = {
+  [Segments.BODY]: Joi.object({
+    email: Joi.string().required().trim(),
+    password: Joi.string().required(),
+  }),
+};
