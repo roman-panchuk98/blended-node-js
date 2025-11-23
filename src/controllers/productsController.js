bb56f98c1c05e020eddfeb3702d9c7cbd1051aab
Beowulf3 <vk3volkov@gmail.com>
2025-11-23 14:26:23 +0200

    add authenticate middleware, add relation between user and product models

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -8,13 +8,16 @@ import {
 } from '../services/product.js';
 
 export const getProducts = async (req, res) => {
-  const products = await getAllProductsService();
+  const products = await getAllProductsService({ userId: req.user._id });
   res.status(200).json(products);
 };
 
 export const getProductById = async (req, res) => {
   const { productId } = req.params;
-  const product = await getProductByIdService(productId);
+  const product = await getProductByIdService({
+    _id: productId,
+    userId: req.user._id,
+  });
   if (!product) {
     throw createHttpError(404, 'Product not found');
   }
@@ -22,13 +25,19 @@ export const getProductById = async (req, res) => {
 };
 
 export const createProduct = async (req, res) => {
-  const product = await createProductService(req.body);
+  const product = await createProductService({
+    ...req.body,
+    userId: req.user._id,
+  });
   res.status(201).json(product);
 };
 
 export const updateProduct = async (req, res) => {
   const { productId } = req.params;
-  const product = await updateProductService(productId, req.body);
+  const product = await updateProductService(
+    { _id: productId, userId: req.user._id },
+    req.body,
+  );
   if (!product) {
     throw createHttpError(404, 'Product not found');
   }
@@ -37,7 +46,10 @@ export const updateProduct = async (req, res) => {
 
 export const deleteProductById = async (req, res) => {
   const { productId } = req.params;
-  const product = await deleteProductByIdService(productId);
+  const product = await deleteProductByIdService({
+    _id: productId,
+    userId: req.user._id,
+  });
   if (!product) {
     throw createHttpError(404, 'Product not found');
   }
